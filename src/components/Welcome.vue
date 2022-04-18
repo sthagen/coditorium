71e8bc48f696b645e9da84b9aaef650db36961c5
Stefan Hagen <stefan@hagen.link>
2022-04-18 14:57:52 +0200

    Updated supported test provider

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -28,7 +28,7 @@ import SupportIcon from './icons/IconSupport.vue'
     This project is served and bundled with
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>. The recommended IDE setup is
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> + <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>. If you
-    need to test your components and web pages, check out <a href="https://www.cypress.io/" target="_blank">Cypress</a> and
+    need to test your components and web pages, check out <a href="https://vitest.dev" target="_blank">Vitest</a> and maybe not
     <a href="https://docs.cypress.io/guides/component-testing/introduction" target="_blank">Cypress Component Testing</a>.
 
     <br />
