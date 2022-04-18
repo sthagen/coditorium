2ce190d91169368b0b6ea34ec2046658507dc7ff
Stefan Hagen <stefan@hagen.link>
2022-04-18 14:50:55 +0200

    Linted for the special team

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string;
-}>();
+  msg: string
+}>()
 
 // Intentional linting warning left over to see if prettier linting works correctly
-
-
-
-
-
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <!-- Intentional linting error left over to see if vue linting works correctly -->
-    <div v-for="(item, index) in [1, 2, 3]" />
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://v3.vuejs.org/">Vue 3</a>.
+      You've successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> + <a target="_blank" href="https://v3.vuejs.org/">Vue 3</a>.
     </h3>
   </div>
 </template>
@@ -38,10 +31,6 @@ h3 {
 /* intentional linting error to see if stylelint is working correctly */
 /* in this case it's the "selector-type-no-unknown" rule */
 
-
-div::v-this-doesnt-exist {
-  width: 5px;
-}
 .greetings h1,
 .greetings h3 {
   text-align: center;
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import WelcomeItem from "./WelcomeItem.vue";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
-import ToolingIcon from "./icons/IconTooling.vue";
-import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
-import SupportIcon from "./icons/IconSupport.vue";
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
@@ -14,7 +14,7 @@ import SupportIcon from "./icons/IconSupport.vue";
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
+    Vue's
     <a target="_blank" href="https://v3.vuejs.org/">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
@@ -26,17 +26,10 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Tooling</template>
 
     This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>.
-    The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>.
-    If you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank">Cypress</a> and
-    <a
-      href="https://docs.cypress.io/guides/component-testing/introduction"
-      target="_blank"
-      >Cypress Component Testing</a
-    >.
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>. The recommended IDE setup is
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> + <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>. If you
+    need to test your components and web pages, check out <a href="https://www.cypress.io/" target="_blank">Cypress</a> and
+    <a href="https://docs.cypress.io/guides/component-testing/introduction" target="_blank">Cypress Component Testing</a>.
 
     <br />
 
@@ -50,16 +43,10 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Ecosystem</template>
 
     Get official tools and libraries for your project:
-    <a target="_blank" href="https://next.vuex.vuejs.org/">Vuex</a>,
-    <a target="_blank" href="https://next.router.vuejs.org/">Vue Router</a>,
-    <a target="_blank" href="https://next.vue-test-utils.vuejs.org/"
-      >Vue Test Utils</a
-    >, and
-    <a target="_blank" href="https://github.com/vuejs/devtools">Vue Dev Tools</a
-    >. If you need more resources, we suggest paying
-    <a target="_blank" href="https://github.com/vuejs/awesome-vue"
-      >Awesome Vue</a
-    >
+    <a target="_blank" href="https://next.vuex.vuejs.org/">Vuex</a>, <a target="_blank" href="https://next.router.vuejs.org/">Vue Router</a>,
+    <a target="_blank" href="https://next.vue-test-utils.vuejs.org/">Vue Test Utils</a>, and
+    <a target="_blank" href="https://github.com/vuejs/devtools">Vue Dev Tools</a>. If you need more resources, we suggest paying
+    <a target="_blank" href="https://github.com/vuejs/awesome-vue">Awesome Vue</a>
     a visit.
   </WelcomeItem>
 
@@ -70,13 +57,9 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Community</template>
 
     Got stuck? Ask your question on
-    <a target="_blank" href="https://chat.vuejs.org">Vue Land</a>, our official
-    Discord server, or
-    <a target="_blank" href="https://stackoverflow.com/questions/tagged/vue.js"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a target="_blank" href="https://news.vuejs.org">our mailing list</a> and
-    follow the official
+    <a target="_blank" href="https://chat.vuejs.org">Vue Land</a>, our official Discord server, or
+    <a target="_blank" href="https://stackoverflow.com/questions/tagged/vue.js">StackOverflow</a>. You should also subscribe to
+    <a target="_blank" href="https://news.vuejs.org">our mailing list</a> and follow the official
     <a target="_blank" href="https://twitter.com/vuejs">@vuejs</a>
     twitter account for latest news in the Vue world.
   </WelcomeItem>
@@ -87,10 +70,7 @@ import SupportIcon from "./icons/IconSupport.vue";
     </template>
     <template #heading>Support Vue</template>
 
-    As an independent project, Vue relies on community backing for its
-    sustainability. You can help us by
-    <a target="_blank" href="https://vuejs.org/support-vuejs/"
-      >becoming a sponsor</a
-    >.
+    As an independent project, Vue relies on community backing for its sustainability. You can help us by
+    <a target="_blank" href="https://vuejs.org/support-vuejs/">becoming a sponsor</a>.
   </WelcomeItem>
 </template>
